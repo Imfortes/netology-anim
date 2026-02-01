d7ea34d6c5fea307decbc71366271af4e2eb3b35
Imfortes <imfortes@list.ru>
2026-02-01 18:35:44 +0300

    fix js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,51 @@
-import Collapse from "./classes/Collapse";
-
 document.addEventListener("DOMContentLoaded", function () {
-  const collapse = new Collapse();
+  class Collapse {
+    constructor() {
+      this.isOpen = false;
+
+      this.buttons = [];
+      this.containers = [];
+
+      this.init();
+    }
+
+    init() {
+      this.buttons = document.querySelectorAll('a[data-toggle="collapse"]');
+      this.containers = document.querySelectorAll(".collapse");
+
+      this.buttons.forEach((btn) => {
+        btn.addEventListener("click", (e) => this.toggle(e));
+      });
+
+      console.log(this.buttons);
+      console.log(this.containers);
+    }
+
+    toggle(e) {
+      const selector = e.currentTarget.dataset.controls;
+      const container = document.querySelector(`#${selector}`);
+
+      if (!container) return;
+
+      const isOpen = container.classList.contains("is-open");
+
+      if (isOpen) {
+        this.close(container);
+      } else {
+        this.open(container);
+      }
+    }
+
+    open(container) {
+      container.classList.add("is-open");
+    }
+
+    close(container) {
+      container.classList.remove("is-open");
+    }
+  }
+
+  new Collapse();
 
   const callbackChatBtn = document.querySelector("#callback-chat-btn");
   const callbackChat = document.querySelector("#callback-chat");
